c266a5770975f8bf2a0e2affa1f8bdd2642bf7ad
Igor01 <Ih01@i.ua>
2017-03-16 19:27:21 +0200

    Библиотека Timers (без сохранения)

--- bin/Release/libraries/Timers/lib.js
+++ bin/Release/libraries/Timers/lib.js
@@ -1,13 +1,50 @@
-﻿var Timers = {};
+﻿var Timers = {
+    Once: new Array(),
+    Repetetive: new Array()
+};
 
 Timers.modTick = function(){
-    
+    this.Once = this.Once.filter(function(item){
+        item.Time--;
+        if(item.Time <= 0){
+            item.Action();
+            return false;
+        }
+        else
+            return true;
+    });
+    this.Repetetive.forEach(function(item){
+        item.Time--;
+        if(item.Time <= 0){
+            item.Action();
+            item.Time = item.Period;
+        }
+    });
 }
 
-Timers.addOnce = function(){
-    print("addOnce");
+Timers.addOnce = function(id, time, action){
+    this.Once.push({Time:time, Action:action});
 }
 
-Timers.addRepetiteve = function(){
-    print("addRepetiteve");
+Timers.addRepetiteve = function(id, time, action){
+    this.Repetetive.push({Id:id, Period:time, Time:time, Action:action});
+}
+
+Timers.remove = function(id){
+    this.Repetetive = this.Repetetive.filter(function(item){
+        if(item.Id == id){
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
+    this.Once = this.Once.filter(function(item){
+        if(item.Id == id){
+            return false;
+        }
+        else{
+            return true;
+        }
+    });
 }
