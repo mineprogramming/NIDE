0f83a7d3baf2ea082743881403909ade09ae73cf
Igor01 <Ih01@i.ua>
2017-07-12 10:14:07 +0300

    2017v4

---
+++ Server/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+            .main{
+                width:200px;
+                height:120px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            <a href="https://www.mineprogramming.org/">Зайдите на сайт #mineprogramming! Начните программировать уже сейчас!</a>
+        </div>
+    </body>
+</html>
