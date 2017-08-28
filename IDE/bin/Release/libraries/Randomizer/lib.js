3d62ddd54f410c5f7d03fc309bb22cd0a03a4e5e
Igor01 <Ih01@i.ua>
2017-08-28 10:30:45 +0300

    Все-таки вернул bin/release

---
+++ IDE/bin/Release/libraries/Randomizer/lib.js
@@ -0,0 +1,21 @@
+﻿var Randomizer = { 
+    Random: (new java.util.Random(Util.getWorldSeed())) 
+};
+Randomizer.ForCoords = function(x, z){
+    var seed = Number(Util.getWorldSeed());
+    var coord = Number(Math.round(x) + "" + Math.abs(Math.round(z)*2));
+    this.Random = new java.util.Random(seed + coord);
+}
+Randomizer.GaussRandom = function(max, depth){
+    var result = 0;
+    for(var i = 0; i < depth; i++){
+        result += this.Random.nextInt(max * 2) - max;
+    }
+    return Math.round(Math.abs(result / depth));
+};
+Randomizer.Double = function(){ 
+    return this.Random.nextDouble(); 
+}
+Randomizer.Int = function(max){ 
+    return this.Random.nextInt(max); 
+}
---
+++ IDE/bin/Release/libraries/Util/lib.js
@@ -0,0 +1,43 @@
+﻿var Util = {}
+
+Util.getWorldSeed = function(){
+    var worldsPath = "/storage/sdcard0/games/com.mojang/minecraftWorlds/";
+    var leveldat = worldsPath+Level.getWorldDir()+"/level.dat";
+    if(!java.io.File(leveldat).exists()){
+        return 0;
+    }
+    var fin = new java.io.FileInputStream(leveldat);
+    var nechs = [];
+    var startSeed = 0;
+    var seed = "";
+    var str = "";
+    var ch;
+    while((ch=fin.read())!=-1){
+        nechs.push(ch);
+        str+=String.fromCharCode(ch);
+    }
+    startSeed = str.split("RandomSeed")[0].length+10;
+    for(var i=3;i>=0;i--){
+        if(nechs[startSeed+i]<16){
+            seed+="0"+nechs[startSeed+i].toString(16)+"";
+        }else{
+            seed+=nechs[startSeed+i].toString(16)+"";
+        }
+    }
+    var endSeed = parseInt(seed, 16);
+    if(endSeed>(Math.pow(16,8)/2-1)){
+        return endSeed-Math.pow(16,8);
+    }
+    return endSeed;
+};
+
+Util.convertStreamToString = function(is) { 
+    var bis = new java.io.BufferedInputStream(is); 
+    var buf = new java.io.ByteArrayOutputStream(); 
+    var res = bis.read(); 
+    while(res != -1) { 
+        buf.write(res); 
+        res = bis.read(); 
+    } 
+    return buf.toString(); 
+}
