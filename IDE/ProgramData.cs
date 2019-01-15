bcef276c804e6afb80d9c836e7a4d09c08cb5f79
IchZerowan <Ih01@i.ua>
2019-01-15 13:21:41 +0200

    Fixed element positions in settings

--- IDE/ProgramData.cs
+++ IDE/ProgramData.cs
@@ -15,7 +15,7 @@ namespace NIDE
 
         public const int PROGRAM_VERSION = 
             /*THIS IS VERY IMPORTANT*/
-            /*Ta-boom-tss...*/     42;
+            /*Ta-boom-tss...*/     44;
             /*EXTREMELY*/
         public const int API_LEVEL = 3;
 
--- IDE/fSettings.Designer.cs
+++ IDE/fSettings.Designer.cs
@@ -65,6 +65,7 @@
             // 
             // groupBox1
             // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.btnComments);
             this.groupBox1.Controls.Add(this.label11);
             this.groupBox1.Controls.Add(this.btnKeywords);
@@ -85,329 +86,192 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.btnNamespaces);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(16, 15);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(439, 239);
-            this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            resources.ApplyResources(this.groupBox1, "groupBox1");
             // 
             // btnComments
             // 
-            this.btnComments.Location = new System.Drawing.Point(331, 130);
-            this.btnComments.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnComments, "btnComments");
             this.btnComments.Name = "btnComments";
-            this.btnComments.Size = new System.Drawing.Size(100, 28);
-            this.btnComments.TabIndex = 19;
             this.btnComments.UseVisualStyleBackColor = true;
             this.btnComments.Click += new System.EventHandler(this.btnComments_Click);
-            resources.ApplyResources(this.btnComments, "btnComments");
             // 
             // label11
             // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(248, 137);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(74, 17);
-            this.label11.TabIndex = 18;
             resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
             // 
             // btnKeywords
             // 
-            this.btnKeywords.Location = new System.Drawing.Point(331, 95);
-            this.btnKeywords.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnKeywords, "btnKeywords");
             this.btnKeywords.Name = "btnKeywords";
-            this.btnKeywords.Size = new System.Drawing.Size(100, 28);
-            this.btnKeywords.TabIndex = 17;
             this.btnKeywords.UseVisualStyleBackColor = true;
             this.btnKeywords.Click += new System.EventHandler(this.btnKeywords_Click);
-            resources.ApplyResources(this.btnKeywords, "btnKeywords");
             // 
             // label10
             // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(252, 101);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(69, 17);
-            this.label10.TabIndex = 16;
             resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
             // 
             // btnStrings
             // 
-            this.btnStrings.Location = new System.Drawing.Point(331, 59);
-            this.btnStrings.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnStrings, "btnStrings");
             this.btnStrings.Name = "btnStrings";
-            this.btnStrings.Size = new System.Drawing.Size(100, 28);
-            this.btnStrings.TabIndex = 15;
             this.btnStrings.UseVisualStyleBackColor = true;
             this.btnStrings.Click += new System.EventHandler(this.btnStrings_Click);
-            resources.ApplyResources(this.btnStrings, "btnStrings");
             // 
             // label9
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(271, 65);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(52, 17);
-            this.label9.TabIndex = 14;
             resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
             // 
             // btnNumbers
             // 
-            this.btnNumbers.Location = new System.Drawing.Point(331, 23);
-            this.btnNumbers.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnNumbers, "btnNumbers");
             this.btnNumbers.Name = "btnNumbers";
-            this.btnNumbers.Size = new System.Drawing.Size(100, 28);
-            this.btnNumbers.TabIndex = 13;
             this.btnNumbers.UseVisualStyleBackColor = true;
             this.btnNumbers.Click += new System.EventHandler(this.btnNumbers_Click);
-            resources.ApplyResources(this.btnNumbers, "btnNumbers");
             // 
             // label8
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(257, 30);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(65, 17);
-            this.label8.TabIndex = 12;
             resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(108, 202);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnBack, "btnBack");
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(100, 28);
-            this.btnBack.TabIndex = 11;
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
-            resources.ApplyResources(this.btnBack, "btnBack");
             // 
             // label7
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(13, 208);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(84, 17);
-            this.label7.TabIndex = 10;
             resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
             // 
             // btnNormal
             // 
-            this.btnNormal.Location = new System.Drawing.Point(108, 23);
-            this.btnNormal.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnNormal, "btnNormal");
             this.btnNormal.Name = "btnNormal";
-            this.btnNormal.Size = new System.Drawing.Size(100, 28);
-            this.btnNormal.TabIndex = 9;
             this.btnNormal.UseVisualStyleBackColor = true;
             this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
-            resources.ApplyResources(this.btnNormal, "btnNormal");
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(20, 30);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(79, 17);
-            this.label6.TabIndex = 8;
             resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
             // 
             // btnMembers
             // 
-            this.btnMembers.Location = new System.Drawing.Point(108, 166);
-            this.btnMembers.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnMembers, "btnMembers");
             this.btnMembers.Name = "btnMembers";
-            this.btnMembers.Size = new System.Drawing.Size(100, 28);
-            this.btnMembers.TabIndex = 7;
             this.btnMembers.UseVisualStyleBackColor = true;
             this.btnMembers.Click += new System.EventHandler(this.btnMembers_Click);
-            resources.ApplyResources(this.btnMembers, "btnMembers");
             // 
             // label4
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(33, 172);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 17);
-            this.label4.TabIndex = 6;
             resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
             // 
             // btnGlobal
             // 
-            this.btnGlobal.Location = new System.Drawing.Point(108, 130);
-            this.btnGlobal.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnGlobal, "btnGlobal");
             this.btnGlobal.Name = "btnGlobal";
-            this.btnGlobal.Size = new System.Drawing.Size(100, 28);
-            this.btnGlobal.TabIndex = 5;
             this.btnGlobal.UseVisualStyleBackColor = true;
             this.btnGlobal.Click += new System.EventHandler(this.btnGlobal_Click);
-            resources.ApplyResources(this.btnGlobal, "btnGlobal");
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(51, 137);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 17);
-            this.label3.TabIndex = 4;
             resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
             // 
             // btnHooks
             // 
-            this.btnHooks.Location = new System.Drawing.Point(108, 95);
-            this.btnHooks.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnHooks, "btnHooks");
             this.btnHooks.Name = "btnHooks";
-            this.btnHooks.Size = new System.Drawing.Size(100, 28);
-            this.btnHooks.TabIndex = 3;
             this.btnHooks.UseVisualStyleBackColor = true;
             this.btnHooks.Click += new System.EventHandler(this.btnHooks_Click);
-            resources.ApplyResources(this.btnHooks, "btnHooks");
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(49, 101);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 17);
-            this.label2.TabIndex = 2;
             resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
             // 
             // btnNamespaces
             // 
-            this.btnNamespaces.Location = new System.Drawing.Point(108, 59);
-            this.btnNamespaces.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnNamespaces, "btnNamespaces");
             this.btnNamespaces.Name = "btnNamespaces";
-            this.btnNamespaces.Size = new System.Drawing.Size(100, 28);
-            this.btnNamespaces.TabIndex = 1;
             this.btnNamespaces.UseVisualStyleBackColor = true;
             this.btnNamespaces.Click += new System.EventHandler(this.btnNamespaces_Click);
-            resources.ApplyResources(this.btnNamespaces, "btnNamespaces");
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 34);
-            this.label1.TabIndex = 0;
-            //this.label1.Text = "Namespaces \r\nand brackets";
             resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
             // 
             // tbPath
             // 
-            this.tbPath.Location = new System.Drawing.Point(512, 145);
-            this.tbPath.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.tbPath, "tbPath");
             this.tbPath.Name = "tbPath";
-            this.tbPath.Size = new System.Drawing.Size(327, 22);
-            this.tbPath.TabIndex = 1;
             // 
             // label5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(508, 97);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(138, 17);
-            this.label5.TabIndex = 2;
             resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
             // 
             // btnApply
             // 
-            this.btnApply.Location = new System.Drawing.Point(739, 226);
-            this.btnApply.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.btnApply, "btnApply");
             this.btnApply.Name = "btnApply";
-            this.btnApply.Size = new System.Drawing.Size(100, 28);
-            this.btnApply.TabIndex = 3;
             this.btnApply.UseVisualStyleBackColor = true;
             this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
-            resources.ApplyResources(this.btnApply, "btnApply");
             // 
             // cbLast
             // 
-            this.cbLast.AutoSize = true;
-            this.cbLast.Location = new System.Drawing.Point(511, 55);
-            this.cbLast.Margin = new System.Windows.Forms.Padding(4);
-            this.cbLast.Name = "cbLast";
-            this.cbLast.Size = new System.Drawing.Size(135, 21);
-            this.cbLast.TabIndex = 4;
-            this.cbLast.UseVisualStyleBackColor = true;
             resources.ApplyResources(this.cbLast, "cbLast");
+            this.cbLast.Name = "cbLast";
+            this.cbLast.UseVisualStyleBackColor = true;
             // 
             // cbRunProgram
             // 
-            this.cbRunProgram.AutoSize = true;
-            this.cbRunProgram.Location = new System.Drawing.Point(511, 197);
-            this.cbRunProgram.Margin = new System.Windows.Forms.Padding(4);
-            this.cbRunProgram.Name = "cbRunProgram";
-            this.cbRunProgram.Size = new System.Drawing.Size(237, 21);
-            this.cbRunProgram.TabIndex = 5;
-            this.cbRunProgram.UseVisualStyleBackColor = true;
             resources.ApplyResources(this.cbRunProgram, "cbRunProgram");
+            this.cbRunProgram.Name = "cbRunProgram";
+            this.cbRunProgram.UseVisualStyleBackColor = true;
             // 
             // cbHighlighting
             // 
-            this.cbHighlighting.AutoSize = true;
-            this.cbHighlighting.Location = new System.Drawing.Point(511, 26);
-            this.cbHighlighting.Margin = new System.Windows.Forms.Padding(4);
+            resources.ApplyResources(this.cbHighlighting, "cbHighlighting");
             this.cbHighlighting.Name = "cbHighlighting";
-            this.cbHighlighting.Size = new System.Drawing.Size(181, 21);
-            this.cbHighlighting.TabIndex = 6;
             this.cbHighlighting.UseVisualStyleBackColor = true;
             this.cbHighlighting.CheckedChanged += new System.EventHandler(this.cbHighlighting_CheckedChanged);
-            resources.ApplyResources(this.cbHighlighting, "cbHighlighting");
             // 
             // rbDevice
             // 
-            this.rbDevice.AutoSize = true;
+            resources.ApplyResources(this.rbDevice, "rbDevice");
             this.rbDevice.Checked = true;
-            this.rbDevice.Location = new System.Drawing.Point(511, 117);
             this.rbDevice.Name = "rbDevice";
-            this.rbDevice.Size = new System.Drawing.Size(72, 21);
-            this.rbDevice.TabIndex = 20;
             this.rbDevice.TabStop = true;
             this.rbDevice.UseVisualStyleBackColor = true;
             this.rbDevice.CheckedChanged += new System.EventHandler(this.rbDevice_CheckedChanged);
-            resources.ApplyResources(this.rbDevice, "rbDevice");
             // 
             // rbEmulator
             // 
-            this.rbEmulator.AutoSize = true;
-            this.rbEmulator.Location = new System.Drawing.Point(617, 117);
+            resources.ApplyResources(this.rbEmulator, "rbEmulator");
             this.rbEmulator.Name = "rbEmulator";
-            this.rbEmulator.Size = new System.Drawing.Size(85, 21);
-            this.rbEmulator.TabIndex = 21;
             this.rbEmulator.UseVisualStyleBackColor = true;
             this.rbEmulator.CheckedChanged += new System.EventHandler(this.rbEmulator_CheckedChanged);
-            resources.ApplyResources(this.rbEmulator, "rbEmulator");
             // 
             // rbCustom
             // 
-            this.rbCustom.AutoSize = true;
-            this.rbCustom.Location = new System.Drawing.Point(729, 117);
+            resources.ApplyResources(this.rbCustom, "rbCustom");
             this.rbCustom.Name = "rbCustom";
-            this.rbCustom.Size = new System.Drawing.Size(76, 21);
-            this.rbCustom.TabIndex = 22;
             this.rbCustom.UseVisualStyleBackColor = true;
             this.rbCustom.CheckedChanged += new System.EventHandler(this.rbCustom_CheckedChanged);
-            resources.ApplyResources(this.rbCustom, "rbCustom");
             // 
             // fSettings
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(859, 270);
             this.Controls.Add(this.rbCustom);
             this.Controls.Add(this.rbEmulator);
             this.Controls.Add(this.rbDevice);
@@ -419,14 +283,11 @@
             this.Controls.Add(this.tbPath);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "fSettings";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
-            resources.ApplyResources(this, "fSettings");
 
         }
 
